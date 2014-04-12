21a8954ba9a73425ac5dd665a680c3a986be7683
Siegfried Puchbauer <S.Puchbauer@spp.at>
2014-04-12 12:18:55 +0200

    some basic logic

--- player.js
+++ player.js
@@ -1,42 +1,66 @@
+var _ = require('./underscore');
+
 module.exports = {
 
     VERSION: "Default JavaScript folding player",
 
-    bet_request: function(game_state) {
-        var players = game_state.players;
-        var current_buy_in = game_state.current_buy_in;
-        
-        switch (parseInt(Math.random() * 10)) {
-            case 0:
-                return this.allIn(game_state);
-            case 1:
-                return this.raiseMin(game_state);
-            case 2:
-                return this.fold(game_state);
-            default:
-                return this.check(game_state);
+    bet_request: function(gameState) {
+        var players = gameState.players;
+        var current_buy_in = gameState.current_buy_in;
+        var me = players[gameState.in_action];
+
+        if (this.isPreflop(gameState)) {
+            if (this.ratePreflop(gameState) > 0) {
+                return 1000;
+            } else {
+                return 0;
+            }
+        } else {
+            switch (parseInt(Math.random() * 10)) {
+                case 0:
+                    return this.allIn(gameState);
+                case 1:
+                    return this.raiseMin(gameState);
+                case 2:
+                    return this.fold(gameState);
+                default:
+                    return this.check(gameState);
+            }
         }
     },
-    
-    raiseMin: function(game_state){
-        var me = game_state.players[game_state.in_action]; 
-        return game_state.current_buy_in - me.bet + game_state.minimum_raise;
+
+    ratePreflop: function(gameState) {
+        var players = gameState.players;
+        var me = players[gameState.in_action];
+        var goodCards = ['J', 'D', 'K', 'A'];
+        
+        return _(me.hole_cards).chain().pluck('rank').all(function(r){
+            return _.contains(goodCards, r);
+        }).value() ? 1 : 0;
     },
 
-
-    allIn: function(game_state){
-        return game_state.players[game_state.in_action].stack;
+    isPreflop: function(gameState) {
+        return !gameState.community_cards.length;
     },
 
-    check: function(game_state){
+    raiseMin: function(gameState) {
+        var me = gameState.players[gameState.in_action];
+        return gameState.current_buy_in - me.bet + gameState.minimum_raise;
+    },
+
+    allIn: function(gameState) {
+        return gameState.players[gameState.in_action].stack;
+    },
+
+    check: function(gameState) {
         return 0;
     },
-    
-    fold: function(game_state){
+
+    fold: function(gameState) {
         return 0;
     },
-    
-    showdown: function(game_state) {
+
+    showdown: function(gameState) {
 
     }
 };
