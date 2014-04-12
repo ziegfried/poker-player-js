d09b8fb6ec4b7598fa7fd7db88c5558829765420
Siegfried Puchbauer <S.Puchbauer@spp.at>
2014-04-12 11:58:51 +0200

    random :)

--- player.js
+++ player.js
@@ -3,9 +3,38 @@ module.exports = {
     VERSION: "Default JavaScript folding player",
 
     bet_request: function(game_state) {
-        return 100;
+        var players = game_state.players;
+        var current_buy_in = game_state.current_buy_in;
+        
+        switch (parseInt(Math.random() * 10)) {
+            case 0:
+                return this.allIn(game_state);
+            case 1:
+                return this.raiseMin(game_state);
+            case 2:
+                return this.fold(game_state);
+            default:
+                return this.check(game_state);
+        }
+    },
+    
+    raiseMin: function(game_state){
+        return game_state.current_buy_in - game_state.players[game_state.in_action][game_state.bet] + game_state.minimum_raise;
     },
 
+
+    allIn: function(game_state){
+        return game_state.players[game_state.in_action].stack;
+    },
+
+    check: function(game_state){
+        return 0;
+    },
+    
+    fold: function(game_state){
+        return 0;
+    },
+    
     showdown: function(game_state) {
 
     }
